e33e0b76cc422c2fe6681809a57b534c9885247b
Minwookim <trte135@naver.com>
2023-07-07 22:33:27 +0900

    #4 Conditional Statement - [IF, Else]
    - Content : 
        1. Conditional Using[if, if-else]

---
+++ conditional/day4/conditional.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    int carrots = 10;
+
+    if (carrots < 5)
+    {
+        cout << "Let's buy some carrots!" << endl;
+    }
+    else
+    {
+        cout << "No need to buy new carrots!" << endl;
+    }
+    
+
+    return 0;
+}
